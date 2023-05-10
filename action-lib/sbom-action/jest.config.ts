f6846c24e615d9b6458dcb42c6cd16d0d1562c80
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2023-05-11 06:45:10 +0700

    up

---
+++ action-lib/sbom-action/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'action-lib-sbom-action',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/action-lib/sbom-action',
+};
